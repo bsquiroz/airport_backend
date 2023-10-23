2108795f772b17bf0e8b511f99948e7819013e82
bsquiroz <bemq1605@gmail.com>
2023-10-23 14:49:02 -0500

    add funcionality flights sockets

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -1,12 +1,20 @@
+const { FlightServices } = require("../modules/flight/flight.services");
+
 class Sockets {
     constructor(io) {
         this.io = io;
         this.socketsEvents();
+        this.flightService = new FlightServices();
     }
 
     socketsEvents() {
         this.io.on("connection", (socket) => {
             console.log("Conectado al servidor");
+
+            socket.on("get-flights", async () => {
+                const flights = await this.flightService.findAll();
+                this.io.emit("news-flights", flights);
+            });
         });
     }
 }
